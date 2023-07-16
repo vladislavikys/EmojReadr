bd389acad9d533f7b335fc1d83aa68e8502db792
vladislavikys <zarya.vlad@yandex.ru>
2023-07-16 09:24:02 +0300

    Initial Commit

---
+++ EmojiReader-001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiReader-001
+//
+//  Created by Влад on 16.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
